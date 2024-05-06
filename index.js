acacf43b1d131b9b88997a7ba05fd1cc3a794574
Saulo Daniel Ferreira Pontes <saulodfp@gmail.com>
2024-05-05 22:03:36 -0300

    Desafio do Classificador de Hérois foi concluido.

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 // Arquivo Inicial
 
-console.log("oi");
+//Classificador de nivel de heroi
+
+console.log("Oi. Bem vindo heroi.");
+
+let nome;
+let xp;
+let nivel;
+
+const readline = require("readline-sync"); 
+
+console.log("Qual o seu nome? ");
+nome = readline.question();
+
+console.log("Qual a sua experiencia?");
+xp = Number(readline.question());
+
+if(xp <= 1000)
+{
+     nivel = 'Ferro';
+}
+else if(xp >= 1001 && xp <= 2000)
+    {
+        nivel = 'Bronze';
+    }
+    else if(xp >= 2001 && xp <= 5000)
+        {
+            nivel = 'Prata';
+        }
+        else if(xp >= 5001 && xp <= 6000)
+            {
+                nivel = 'Ouro';
+            }
+            else if(xp >= 6001 && xp <= 9000)
+                {
+                    nivel = 'Platina Diamante';
+                }
+                else if(xp >= 8001 && xp <= 9000)
+                    {
+                        nivel = 'Ascendente';
+                    }
+                    else if(xp >= 9001 && xp <= 10000)
+                        {
+                            nivel = 'Imortal';
+                        }
+                        else if(xp >= 10001)
+                            {
+                                nivel = 'Radiante';
+                            }
+
+console.log('O Herói de nome '+nome+' está no nivel de '+nivel+'!');
+
